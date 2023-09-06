4989db19a95ba073bd7da941ef1795d982d615fe
sunjin <changido2888@gmail.com>
2023-09-07 00:45:33 +0900

    [ADD] components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import { useReducer } from "react";
+
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import RouteTest from "./components/RouteTest";
 
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const newItem = {
+        ...action.data,
+      };
+      newState = [newItem, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  // CREATE
+  // REMOVE
+  // EDIT
   return (
     <BrowserRouter>
       <div className="App">
         <h2>app.js</h2>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
