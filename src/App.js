c0f385e4c036c2876af85ecffcd81f1fb5c72828
sunjin <changido2888@gmail.com>
2023-09-26 01:01:57 +0900

    수정 중

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -37,25 +37,99 @@ const reducer = (state, action) => {
   return newState;
 };
 
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date: 1694150945320,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date: 1694150945321,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date: 1694150945322,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date: 1694150945323,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date: 1694150945324,
+  },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
+
+  const dataId = useRef(0);
+
+  console.log(new Date().getTime());
 
   // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
   // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
   // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h2>app.js</h2>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider
+        value={{
+          onCreate,
+          onEdit,
+          onRemove,
+        }}
+      >
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
---
+++ src/components/Emotionitem.js
@@ -0,0 +1,44 @@
+import React from "react";
+// const EmotionItem = ({
+//   emotion_id,
+//   emotion_img,
+//   emotion_descript,
+//   onClick,
+//   isSelected,
+// }) => {
+//   return (
+//     <div
+//       onClick={() => onClick(emotion_id)}
+//       className={[
+//         "EmotionItem",
+//         isSelected ? `EmotioItem_on_${emotion_id}` : `EmotionItem_off`,
+//       ].join(" ")}
+//     >
+//       <img src={emotion_img} />
+//       <span>{emotion_descript}</span>
+//     </div>
+//   );
+// };
+
+const EmotionItem = ({
+  emotion_id,
+  emotion_img,
+  emotion_descript,
+  onClick,
+  isSelected,
+}) => {
+  return (
+    <div
+      onClick={() => onClick(emotion_id)}
+      className={[
+        "EmotionItem",
+        isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`,
+      ].join(" ")}
+    >
+      <img src={emotion_img} alt="감정 이미지" />
+      <span>{emotion_descript}</span>
+    </div>
+  );
+};
+
+export default EmotionItem;
